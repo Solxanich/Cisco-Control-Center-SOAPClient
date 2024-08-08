404fda4488adaf08f530ca7c2f0225a951a26568
Damola-Atco <damola.akinola@atco.com>
2024-08-08 10:42:05 -0600

    Revert "Implement Logging"

--- SoapClient/Program.cs
+++ SoapClient/Program.cs
@@ -6,9 +6,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
-using log4net;
-using log4net.Config;
-using System.IO;
 
 namespace SoapClient
 {
@@ -17,15 +14,9 @@ namespace SoapClient
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        /// 
-        public static readonly ILog log = LogManager.GetLogger(typeof(Program));
-
         [STAThread]
         static void Main(string[] args)
         {
-            XmlConfigurator.Configure(new FileInfo("LogConfig.xml"));
-            log.Info("\n\nProgram start");
-
             var parsed = Parser.Default.ParseArguments<Options>(args);
             var options = parsed.Value;
 
@@ -33,30 +24,17 @@ namespace SoapClient
             Trace.AutoFlush = true;
             Trace.Indent();
 
-            try
-            {
-                if (options.task == null)
-                    throw new ArgumentNullException("Missing '-task' argument. Options include: 'provision'");
-            }
-            catch(Exception ex) {
-                log.Info(ex.ToString());
-            }
+            if (options.task == null)
+                throw new ArgumentNullException("Missing '-task' argument. Options include: 'provision'");
 
             var service = Shared.GetTerminalService(options.uname, options.apikey);
 
             if (options.task == "provision")
             {
-                try
-                {
-                    if (options.ipfile is null || !options.ipfile.Contains(".csv"))
-                        throw new ArgumentNullException("Missing '-ipfile' argument. Please provide a valid CSV file");
-                    TaskRunner.SetIp(options.ipfile, service, options.license);
-                }
-                catch (Exception ex)
-                {
-                    log.Info(ex.ToString());
-                }
+                if (options.ipfile is null || !options.ipfile.Contains(".csv"))
+                    throw new ArgumentNullException("Missing '-ipfile' argument. Please provide a valid CSV file");
 
+                TaskRunner.SetIp(options.ipfile, service, options.license);
             }
             
             //Application.EnableVisualStyles();
